3007e80a0f57a73d34b0664a695e8a1e9b268b50
Wesley Yao <wesyao@microsoft.com>
2017-11-15 15:32:10 -0800

    add get docs by userid

--- app/services/db/permission.js
+++ app/services/db/permission.js
@@ -1,7 +1,7 @@
 const Permission = require('./models/permission')
 
 /*
-* Get all Documents
+* Get all Permissions
 * @async
 * @returns {Promise<[(User|Array)]>} users - array of mongoose user object
 */
@@ -13,7 +13,16 @@ const getAll = () => {
 }
 
 /*
-* Get Document by mongo _id
+* Get Documents by UserId
+* @async
+* @returns {Promise<[(User|Array)]>} users - array of mongoose document object
+*/
+const getDocumentsByUserId = () => {
+  throw new Error('Not implemented')
+}
+
+/*
+* Get Permission by mongo _id
 * @async
 * @param {string} _id - mongo id for user object
 * @returns {Promise<User>} user - mongoose user object
@@ -26,19 +35,20 @@ const getById = (_id) => {
 }
 
 /*
-* Create Document
+* Create Permission
 * @async
 * @param {string} blobUri - blobUri for this document
 * @returns {Promise<User>} user - mongoose user object
 */
-const create = (blobUri) => {
+const create = (user, document) => {
   return Permission.create({
-    blobUri: blobUri
+    user: user,
+    document: document
   })
 }
 
 /*
-* Update a Document by mongo unique Id
+* Update a Permission by mongo unique Id
 * @async
 * @param {string} _id - mongo id for user object
 * @param {Object} fields - the fields on the user to update
@@ -50,7 +60,7 @@ const update = (_id, fields) => {
 }
 
 /*
-* Remove Document by mongo unique Id
+* Remove Permission by mongo unique Id
 * @async
 * @param {string} _id - mongo id for user object
 * @returns {Promise<Object>} confirmation - mongoose deletion confirmation
@@ -64,6 +74,7 @@ const remove = async (_id) => {
 module.exports = {
   getAll,
   getById,
+  getDocumentsByUserId,
   create,
   update,
   remove
